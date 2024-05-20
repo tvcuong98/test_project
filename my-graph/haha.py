fixing the easy_ml_train : It is training 1 model in all folds (it is bad, data leak)

my-graph/haha.py
@@ -13,6 +13,8 @@ import glob
 from display_utils import display_dataset_discription
 from disk_utils import delete_all_files_in_folder
 from data_utils import pre_normalize_feature_label
+import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 ### Dealing with dataset:
 root = "/home/edabk/cuong/test_project/my-graph/datasets/qm7"
 processed_path="/home/edabk/cuong/test_project/my-graph/datasets/qm7/processed"
@@ -53,22 +55,24 @@ for fold in range(5):
     print(f'Number of graphs: {len(fold_test)}')
 
 
-    train_loader = torch_geometric.loader.DataLoader(fold_train, batch_size=64, shuffle=True)
-    test_loader = torch_geometric.loader.DataLoader(fold_test, batch_size=64, shuffle=False)
-    for epoch in range(100000):
+    train_loader = torch_geometric.loader.DataLoader(fold_train, batch_size=1024, shuffle=True)
+    test_loader = torch_geometric.loader.DataLoader(fold_test, batch_size=1024, shuffle=False)
+    for epoch in range(1000):
         model.train()
+        running_train_loss = 0.0
+        step =0
         for step, data in enumerate(train_loader):
             data.to(device)
-            # print(data.ptr)
-            # print(data.batch)
             optimizer.zero_grad()
             out = model(data)
-            # print(out.shape)
             loss =nn.MSELoss()(out, data.y)
+            running_train_loss+=loss
             loss.backward()
             optimizer.step()
+            step+=1
+        epoch_train_loss = running_train_loss/step
         if epoch%1 == 0:
-            print(f"train:{loss}")
+            step=0
             model.eval()
             running_test_loss=0.0
             for step, data in enumerate(test_loader):
@@ -76,5 +80,30 @@ for fold in range(5):
                 out = model(data)
                 loss = nn.MSELoss()(out,data.y)
                 running_test_loss+=loss
-            print(f"test:{running_test_loss}")
+                step+=1
+            epoch_test_loss = running_test_loss/step
+            print(f"Epoch_{epoch}: train_loss_{epoch_train_loss} test_loss_{epoch_test_loss}")
+    model.eval()
+    running_val_loss = []
+    running_val_mse =[]
+    running_val_rmse =[]
+    running_val_r2 =[]
+    running_val_mae =[]
+    for step, data in enumerate(test_loader):
+        data.to(device)
+        optimizer.zero_grad()
+        out = model(data).cpu().detach()
+        label = data.y.cpu().detach()
+        val_loss = nn.MSELoss()(out, label)
+        running_val_loss.append(val_loss)
+        running_val_mse.append(mean_squared_error(label, out))
+        running_val_rmse.append(np.sqrt(mean_squared_error(label, out)))
+        running_val_r2.append(r2_score(label, out))
+        running_val_mae.append(mean_absolute_error(label,out))
+
+    print(f"  MSE LOSS VAL: {np.mean(running_val_loss):.4f}")
+    print(f"  Mean Squared Error: {np.mean(running_val_mse):.4f}")
+    print(f"  Root Mean Squared Error: {np.mean(running_val_rmse):.4f}")
+    print(f"  R-squared: {np.mean(running_val_r2):.4f}")
+    print(f"  Mean Absolute Error: {np.mean(running_val_mae):.4f}")
 

my-graph/models/GraphSage.py
@@ -48,7 +48,7 @@ class GraphSage(MessagePassing):
         out = torch_scatter.scatter(inputs, index, node_dim, dim_size=dim_size, reduce='mean') # see https://pytorch-scatter.readthedocs.io/en/latest/functions/scatter.html#torch_scatter.scatter
         return out
 class GraphSageRegression(nn.Module):
-    def __init__(self, in_channels,out_channels,hid_channels=64,normalize = True):
+    def __init__(self, in_channels,out_channels,hid_channels=64,normalize = False):
         
         super().__init__()
          

my-regression/easy_ml_train.py
@@ -69,7 +69,7 @@ def train(name,model, split):
             scaler.step(optimizer)
             scaler.update()
             model.zero_grad(set_to_none=True)
-            print(loss)
+            # print(loss)
     model.eval()
     with torch.no_grad():
         y_test_pred= model(x_test)
@@ -112,45 +112,60 @@ def train_ml(name,model, split):
 
 
 # Define the models:
-models = {
+models = ["Multilayer Perceptron","Linear Regression","Support Vector Regression","Kernel Ridge Regression","Gaussian Process Regression"]
     #PyTorch-based MLP for GPU acceleration
     # "Linear Regression": LinearRegression(),
-    "Multilayer Perceptron": torch.nn.Sequential(
-        torch.nn.Linear(276, 256),
-        torch.nn.ReLU(),
-        torch.nn.Linear(256, 256),
-        torch.nn.ReLU(),
-        torch.nn.Linear(256, 128),
-        torch.nn.ReLU(),
-        torch.nn.Linear(128, 1)),
-    "Support Vector Regression": SVR(kernel="rbf"),
-    "Gaussian Process Regression": GaussianProcessRegressor(kernel=RBF(), random_state=0),
-    "Kernel Ridge Regression": KernelRidge(alpha=1.0, kernel='rbf'),  # Add KRR
-}
+#     "Multilayer Perceptron": torch.nn.Sequential(
+#         torch.nn.Linear(276, 256),
+#         torch.nn.ReLU(),
+#         torch.nn.Linear(256, 256),
+#         torch.nn.ReLU(),
+#         torch.nn.Linear(256, 128),
+#         torch.nn.ReLU(),
+#         torch.nn.Linear(128, 1)),
+#     "Support Vector Regression": SVR(kernel="rbf"),
+#     "Gaussian Process Regression": GaussianProcessRegressor(kernel=RBF(), random_state=0),
+#     "Kernel Ridge Regression": KernelRidge(alpha=1.0, kernel='rbf'),  # Add KRR
+# }
 
 # Create output directory if it doesn't exist
 output_dir = 'my-regression/output_ml'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-for name, model in models.items():
+for name in models:
     mse_list =[]
     rmse_list = []
     r2_list=[]
     mae_list=[]
     for fold in range(5):
         if name == "Multilayer Perceptron":
+            model=torch.nn.Sequential(
+                    torch.nn.Linear(276, 256),
+                    torch.nn.ReLU(),
+                    torch.nn.Linear(256, 256),
+                    torch.nn.ReLU(),
+                    torch.nn.Linear(256, 128),
+                    torch.nn.ReLU(),
+                    torch.nn.Linear(128, 1))
             mse,rmse,r2,mae = train(name,model,fold)
-            mse_list.append(mse)
-            rmse_list.append(rmse)
-            r2_list.append(r2)
-            mae_list.append(mae)
-        else:
+
+        elif name == "Linear Regression":
+            model = LinearRegression()
             mse,rmse,r2,mae = train_ml(name,model,fold)
-            mse_list.append(mse)
-            rmse_list.append(rmse)
-            r2_list.append(r2)
-            mae_list.append(mae)
+        elif name == "Support Vector Regression":
+            model = SVR(kernel="rbf")
+            mse,rmse,r2,mae = train_ml(name,model,fold)
+        elif name == "Kernel Ridge Regression":
+            model =  KernelRidge(alpha=1.0, kernel='rbf')
+            mse,rmse,r2,mae = train_ml(name,model,fold)
+        elif name == "Gaussian Process Regression":
+            model =  GaussianProcessRegressor(kernel=RBF(), random_state=0)
+            mse,rmse,r2,mae = train_ml(name,model,fold)
+        mse_list.append(mse)
+        rmse_list.append(rmse)
+        r2_list.append(r2)
+        mae_list.append(mae)
     print(f"\n{name}:")
     print(f"  Mean Squared Error: {np.mean(mse_list):.4f}")
     print(f"  Root Mean Squared Error: {np.mean(rmse_list):.4f}")